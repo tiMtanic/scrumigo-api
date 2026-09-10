bb4e463f56c3592dac9f170e192067e350b43712
Tim Lehming <timlehming@aol.com>
2026-09-10 10:51:05 +0200

    Add user model and authentication routes

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+function verifyToken(req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.payload = payload;
+    next();
+  } catch (error) {
+    res
+      .status(401)
+      .json({ errorMessage: "No valid token" });
+  }
+}
+
+export { verifyToken };
--- models/User.model.js
+++ models/User.model.js
@@ -2,24 +2,35 @@ import { Schema, model } from "mongoose";
 
 const userSchema = new Schema(
   {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 100,
+    },
+    surname: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 100,
+    },
     email: {
       type: String,
-      required: [true, "Email is required."],
+      required: true,
       unique: true,
       lowercase: true,
       trim: true,
+      maxLength: 100,
     },
-    password: {
+    passwordHash: {
       type: String,
-      required: [true, "Password is required."],
+      required: true,
     },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   },
 );
 
 const User = model("User", userSchema);
-
 export default User;
